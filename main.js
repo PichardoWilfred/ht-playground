27d84963661aeb5ae8c234bafa6cdf0b8d7af692
PichardoWilfred <pichardowilfred@gmail.com>
2023-01-19 17:06:22 -0400

    -- dark-mode toggler functional -- palette updated -- gray-unsoported for multiple modes

--- main.js
+++ main.js
@@ -66,9 +66,6 @@ const see_more = {
 handle_limited(see_more['description']);
 handle_limited(see_more['subtasks']);
 
-// taskDrawer interaction
-
-// task-taskDrawer
 
 const taskDrawer = $('#task-drawer');
 // setting the initial value
@@ -101,3 +98,26 @@ const toggleTaskDrawer = () => {
 
 $('#toggle-task-drawer').click(toggleTaskDrawer);
 $('#close-task-drawer').click(toggleTaskDrawer);
+
+
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") { // it will add it lastly no matter the cost
+    document.body.classList.toggle("dark-theme");
+} else if (currentTheme == "light") {
+    document.body.classList.toggle("light-theme");
+}
+
+$('#toggle-dark-mode').click( function() {
+    let theme;
+    if (prefersDarkScheme.matches) { // asks the operating system
+        document.body.classList.toggle("light-theme");
+        theme = document.body.classList.contains("light-theme") ? "light" : "dark";
+    } else {
+        document.body.classList.toggle("dark-theme");
+        theme = document.body.classList.contains("dark-theme") ? "dark" : "light";
+    }
+    localStorage.setItem("theme", theme);
+});
+
