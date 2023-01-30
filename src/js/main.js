08b0585ce4f5a72eac4d13fc6113d999d6491a2b
PichardoWilfred <pichardowilfred@gmail.com>
2023-01-30 14:59:02 -0400

    routing with script and styles completed, missing module importing from within

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 import '../scss/main.scss';
-import home from '../views/home.html'
+import home from '../views/home.html';
+import task from '../views/task-drawer.html'
 // import './router.js'
 // // we need a princpial routing component
 // // import { router } from "./router.js";
 
 // // whenever the apps starts or its 
-
 // const navigateTo = (route) => {
 //     console.log(route);
 //     const router = document.querySelector('router-view');
@@ -19,39 +19,54 @@ import home from '../views/home.html'
 // };
 
 // window.addEventListener("load", init);
+const router = {
+    'home': home,
+    'task-drawer': task,
+}
 
 function initComponent (view) {
     const parser = new DOMParser();
-    const fragment = parser.parseFromString(home, 'text/html');
-    const originalTemplate = fragment.getElementsByTagName('template')[0];
-    let template;
-    if (originalTemplate) {
-        template = document.createElement('template');
-        template.innerHTML = originalTemplate.innerHTML;
-        // template.id = originalTemplate.id;
+    const file = parser.parseFromString(router[view], 'text/html');
+    // console.log(file);
+    const file_template = file.getElementsByTagName('template')[0];
+    if (file_template) {
+        const new_template = document.createElement('template');
+        new_template.innerHTML = file_template.innerHTML;
+        // template.id = file_template.id;
         const router = document.querySelector('#router-view');
-        router.innerHTML = '';
-        router.appendChild(template.content);
+        const content = router.querySelector('#view');
+        console.log(content);
+        router.replaceChild(new_template.content, content);
+    }else {
+        // console.log('');
+        // document.body.innerHTML = '';
     }
     
-    const originalScript = fragment.getElementsByTagName('script')[0];
-    let script;
-    if (originalScript) {
-        script = document.createElement('script');
-        script.innerHTML = originalScript.innerHTML;
-        const currentScript = document.getElementsByTagName('script')[0];
-        currentScript.innerHTML = '';
-        document.body.appendChild(script);
+    const file_script = file.getElementsByTagName('script')[0];
+    if (file_script) {
+        const new_script = document.createElement('script');
+        new_script.id = 'script-tag';
+        new_script.innerHTML = file_script.innerHTML;
+        const current_script = document.querySelector('#script-tag');
+        if (current_script) current_script.remove();
+        document.body.appendChild(new_script);
+    }else {
+        // const current_script = document.getElementsByTagName('script')[0];
+        // current_script.innerHTML = '';
     }
-    const originalStyles = fragment.getElementsByTagName('style')[0];
-    let styles;
-    if (originalStyles) {
-        styles = document.createElement('style');
-        styles.innerHTML = originalStyles.innerHTML;
-        const currentStyles = document.getElementsByTagName('style')[0];
-        currentStyles.innerHTML = '';
-        document.body.appendChild(script);
-    }   
+
+    const file_style = file.getElementsByTagName('style')[0];
+    if (file_style) {
+        const new_styles = document.createElement('style');
+        new_styles.id = 'style-tag';
+        new_styles.innerHTML = file_style.innerHTML;
+        const current_styles = document.querySelector('#style-tag');
+        if (current_styles) current_styles.remove()
+        document.body.appendChild(new_styles);
+    } else {
+        // const current_styles = document.getElementsByTagName('style')[0];
+        // current_styles.innerHTML = '';
+    }
 
 }
 
@@ -62,6 +77,7 @@ const navigateTo = (route) => {
     }else {
         route_ = route.slice(2);
     };
+    console.log(route_);
     initComponent(route_);
 }
 const init = () => {
