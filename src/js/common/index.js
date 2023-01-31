ef81d2a9cc286e0d144d15869bfb6f1b4202e46f
PichardoWilfred <pichardowilfred@gmail.com>
2023-01-31 19:41:26 -0400

    _id & hash values added.

--- src/js/common/index.js
+++ src/js/common/index.js
@@ -1,4 +1,4 @@
 
 //file used for tools and utilities within the app.
-const hash = window.location.hash;
-const _id = hash.substring(hash.lastIndexOf('/') + 1, hash.length);
+const hash = () => window.location.hash;
+const _id = () => hash().substring(hash().lastIndexOf('/') + 1, hash().length);
