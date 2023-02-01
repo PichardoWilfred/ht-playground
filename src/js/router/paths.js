dec3a6d0880df55c0c19ca3a59e87d0db782ecca
John Rodriguez <43676480+jismah@users.noreply.github.com>
2023-02-01 13:14:46 -0400

    Changes

--- src/js/router/paths.js
+++ src/js/router/paths.js
@@ -2,6 +2,7 @@
 import home from '../../views/home.html';
 import task from '../../views/task-drawer.html';
 import global from '../../views/global-search.html';
+import navbar from '../../views/navbar.html';
 
 import files from '../../views/files/index.html';
 import files_archives from '../../views/files/archives.html';
@@ -12,6 +13,7 @@ const routes = {
         'home': home,
         'task-drawer': task,
         'global-search': global,
+        'navbar': navbar,
         'files': {
             '/': files,
             'archives': files_archives,
