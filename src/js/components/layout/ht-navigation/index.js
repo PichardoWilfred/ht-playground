46e5152bc40d70324c81d6b06169ad980dfb6e90
PichardoWilfred <pichardowilfred@gmail.com>
2023-02-01 12:02:56 -0400

    ht-navigation web-component added.

--- src/js/components/layout/ht-navigation/index.js
+++ src/js/components/layout/ht-navigation/index.js
@@ -1,29 +1,5 @@
 import template from './template.html';
 
-// dark-mode
-const toggle_dark_mode = $('#toggle-dark-mode');
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-toggle_dark_mode.text(prefersDarkScheme.matches ? '🌞':'🌙');
-
-const currentTheme = localStorage.getItem("theme");
-if (currentTheme == "dark") { // it will add it lastly no matter the cost
-    document.body.classList.toggle("dark-theme");
-} else if (currentTheme == "light") {
-    document.body.classList.toggle("light-theme");
-}
-
-toggle_dark_mode.click(() => {
-    let theme;
-    if (prefersDarkScheme.matches) { // asks the operating system
-        document.body.classList.toggle("light-theme");
-        theme = document.body.classList.contains("light-theme") ? "light" : "dark";
-    } else {
-        document.body.classList.toggle("dark-theme");
-        theme = document.body.classList.contains("dark-theme") ? "dark" : "light";
-    }
-    localStorage.setItem("theme", theme);
-    toggle_dark_mode.text(theme === 'dark' ? '🌞':'🌙');
-});
 
 // THIS GOES ONTO THE NAV COMPONENT
 $('#reset-path').click(() => {
@@ -36,7 +12,6 @@ class NavigationBar extends HTMLElement {
     constructor(){
         super();
         this.template = template.content;
-        // this.template.innerHTML = home;
         this.path = this.getAttribute("path");
     }
 
@@ -48,9 +23,36 @@ class NavigationBar extends HTMLElement {
         if (attr === 'path') this.path = newV;
     }
 
+    handleDarkMode(){
+        // dark-mode
+        const toggle_dark_mode = $('#toggle-dark-mode');
+        const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+        toggle_dark_mode.text(prefersDarkScheme.matches ? '🌞':'🌙');
+
+        const currentTheme = localStorage.getItem("theme");
+        if (currentTheme == "dark") { // it will add it lastly no matter the cost
+            document.body.classList.toggle("dark-theme");
+        } else if (currentTheme == "light") {
+            document.body.classList.toggle("light-theme");
+        }
+
+        toggle_dark_mode.click(() => {
+            let theme;
+            if (prefersDarkScheme.matches) { // asks the operating system
+                document.body.classList.toggle("light-theme");
+                theme = document.body.classList.contains("light-theme") ? "light" : "dark";
+            } else {
+                document.body.classList.toggle("dark-theme");
+                theme = document.body.classList.contains("dark-theme") ? "dark" : "light";
+            }
+            localStorage.setItem("theme", theme);
+            toggle_dark_mode.text(theme === 'dark' ? '🌞':'🌙');
+        });
+    }
+
     connectedCallback(){
         this.innerHTML = template;
-
+        this.handleDarkMode();
     }
 }
 customElements.define('ht-navigation', NavigationBar);
