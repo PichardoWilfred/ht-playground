7ef38d854cf64b12a7e12720c4a4e6065b2ee6c4
PichardoWilfred <pichardowilfred@gmail.com>
2023-02-10 19:24:19 -0400

    copy of customer-view

--- src/js/router/paths.js
+++ src/js/router/paths.js
@@ -5,6 +5,7 @@ import global from '../../views/global-search.html';
 import navbar from '../../views/navbar.html';
 import contractConfirm from '../../views/contract-confirm.html';
 import customerView from '../../views/customer-view.html';
+import legacyCustomerView from '../../views/legacy-customer-view.html';
 
 import files from '../../views/files/index.html';
 import files_archives from '../../views/files/archives.html';
@@ -12,17 +13,18 @@ import files_detail from '../../views/files/_detail.html';
 import files_deleted from '../../views/files/deleted.html';
 
 const routes = {
-        'home': home,
-        'task-drawer': task,
-        'global-search': global,
-        'navbar': navbar,
-        'customer-view': customerView,
-        'contract-confirm': contractConfirm,
-        'files': {
-            '/': files,
-            'archives': files_archives,
-            'deleted': files_deleted,
-            ':id': files_detail
-        }
+    'home': home,
+    'task-drawer': task,
+    'global-search': global,
+    'navbar': navbar,
+    'legacy-customer-view': legacyCustomerView,
+    'customer-view': customerView,
+    'contract-confirm': contractConfirm,
+    'files': {
+        '/': files,
+        'archives': files_archives,
+        'deleted': files_deleted,
+        ':id': files_detail
+    }
 }
 export { routes }
